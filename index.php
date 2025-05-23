a60d70678b3a7d6a4e31c35f05acc70b42993785
Jayson Nacional <jayson.o.nacional@gmail.com>
2025-05-23 23:07:55 +0800

    added fetch operation

--- index.php
+++ index.php
@@ -1,31 +1,54 @@
-<?php
+<!doctype html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
+		<title>Daily Pomodoro</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" 
+			rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" 
+			crossorigin="anonymous">
+	</head>
+	<body>
+		<?php
 
-include __DIR__ . "/vendor/autoload.php";
+        use Dotenv\Dotenv;
 
-use Dotenv\Dotenv;
-use JaysonNacional\DailyPomodoro\classes\Todos;
+		include __DIR__ . "/vendor/autoload.php";
 
-echo "Welcome to the Daily Pomodoro App. Your partner for productivity!</br>";
+		$dotnev = Dotenv::createImmutable(__DIR__);
+		$dotnev->load();
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+		try {
+		    $pdo = new PDO(dsn: $_ENV["DSN"], username: $_ENV["USER_NAME"], password: $_ENV["PASSWORD"]);
+		    $query = $pdo->query("SELECT * FROM todos");
+		    $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-if ($_ENV["ENVIRONMENT"] == "Development") {
-    error_reporting(E_ALL);
-    ini_set("display_errors", "1");
-}
+		    foreach ($result as $row) {
+		        echo $row['name'], "</br>";
+		    }
 
-$dsn = $_ENV["DSN"];
-$username = $_ENV["USER_NAME"];
-$password = $_ENV["PASSWORD"];
+		} catch (Exception $e) {
+		    echo $e->getMessage();
+		}
+		?>
 
-try {
-    $pdo = new PDO(dsn: $dsn, username: $username, password: $password);
-
-    $todo = new Todos();
-    $todo->create();
-    $todo->update();
-    $todo->delete();
-} catch (Exception $e) {
-    echo $e->getMessage(), "</br>";
-}
+		<div class="card">
+			<div class="card-body">
+				Task 1
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-body">
+				Task 2
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-body">
+				Task 3
+			</div>
+		</div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" 
+			integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" 
+			crossorigin="anonymous"></script>
+	</body>
+</html>
